a844bc7c7c2f1b86ba67b9fb7b6089dd5679069f
williamdothardy33 <william.hardy33@yahoo.com>
2024-07-05 17:49:37 -0400

    added code to specify start of managed array

--- int_buffer_source/int_buffer.c
+++ int_buffer_source/int_buffer.c
@@ -3,10 +3,11 @@
 #include <string.h>
 #include <stdio.h>
 
-int_buf *create_int_buf() {
+int_buf *create_int_buf(int start) {
     int_buf *buf = malloc(sizeof(int_buf));
     int *new_buf = calloc(4, sizeof(int));
-    buf->offset = 0;
+    buf->start = start;
+    buf->offset = start;
     buf->data = new_buf;
     buf->length = 4;
     return buf;
@@ -24,7 +25,7 @@ void append(int_buf *buf, int n) {
 
 int *take_last(int_buf *buf, int count) {
     int *result = NULL;
-    if (buf->offset - count >= 0) {
+    if (buf->offset - count >= buf->start) {
         result = malloc(count * sizeof(int));
         int new_offset = buf->offset - count;
         memcpy(result, buf->data + new_offset, count * sizeof(int));
@@ -35,8 +36,8 @@ int *take_last(int_buf *buf, int count) {
 
 
 
-void show_buf(int_buf *buf) {
-    int offset = 0;
+void show_buf(int_buf *buf, int start) {
+    int offset = start;
     while (offset < buf->offset) {
         printf("number %d at offset %d\n", *(buf->data + offset), offset);
         offset++;
@@ -44,7 +45,7 @@ void show_buf(int_buf *buf) {
 }
 
 int not_empty(int_buf *buf) {
-    int result = buf->offset;
+    int result = buf->offset - buf->start;
     return result;
 }
 
--- int_buffer_source/int_buffer.h
+++ int_buffer_source/int_buffer.h
@@ -4,13 +4,14 @@ struct int_buf {
     int length;
     int offset;
     int *data;
+    int start;
 };
 
 typedef struct int_buf int_buf;
 
-int_buf *create_int_buf();
+int_buf *create_int_buf(int start);
 void append(int_buf *buf, int n);
 int *take_last(int_buf *buf, int count);
-void show_buf(int_buf *buf);
+void show_buf(int_buf *buf, int start);
 int *peek_last(int_buf *buf);
 int not_empty(int_buf *buf);
