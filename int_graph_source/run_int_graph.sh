0e1de58d4537ee93428e25c2861d48c289d3e87c
williamdothardy33 <william.hardy33@yahoo.com>
2024-09-19 23:35:59 -0400

    added modified stack impl obect file to run script

--- int_graph_source/run_int_graph.sh
+++ int_graph_source/run_int_graph.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cc -c int_graph.c int_graph_test.c
-cc -o int_graph int_graph.o int_graph_test.o ../int_queue_source/int_queue.o
+cc -o int_graph int_graph.o int_graph_test.o ../int_queue_source/int_queue.o ../int_stack_source_v2/int_stack.o
 ./int_graph
